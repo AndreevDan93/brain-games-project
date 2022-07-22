14b64eea8ab1ccabe9337624be1cde6d4a26583c
AndreevDan93 <andreevdan93@gmail.com>
2022-07-22 18:26:24 +0500

    add 1st game

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,27 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+        int inputNum;
+        System.out.println("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "0 - Exit");
+        inputNum = scanner.nextInt();
+        System.out.println("Your choice: " + inputNum);
+        if (inputNum == 0) {
+            return;
+        }
+        if (inputNum == 1) {
+            Cli.greeting();
+        }
+        if (inputNum == 2) {
+            ParityCheckGame.game();
+        }
+
+
     }
 }
---
+++ src/main/java/hexlet/code/ParityCheckGame.java
@@ -0,0 +1,47 @@
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ParityCheckGame {
+    public static void game() {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int countCorrectAnswer = 0;
+        Cli.greeting();
+        int countForWin = 3;
+        int randomRange = 100;
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        for (int i = 1; i <= countForWin; i++) {
+
+            int randomNum = random.nextInt(randomRange);
+            boolean flag = randomNum % 2 == 0;
+            System.out.println("Question: " + randomNum);
+            String inputAnswer = scanner.nextLine();
+            if ((flag && inputAnswer.equals("yes")) || (!flag && inputAnswer.equals("no"))) {
+                System.out.println("Correct!");
+                countCorrectAnswer++;
+                continue;
+            }
+            if (flag && !inputAnswer.equals("yes")) {
+                System.out.println("'" + inputAnswer + "' "
+                        + "is wrong answer ;(. Correct answer was 'no'. Let's try again, " + Cli.getName() + "!");
+                break;
+            }
+            if (!flag && !inputAnswer.equals("no")) {
+                System.out.println("'" + inputAnswer + "' "
+                        + "is wrong answer ;(. Correct answer was 'yes'. Let's try again, " + Cli.getName() + "!");
+                break;
+            }
+
+        }
+
+        if (countCorrectAnswer == countForWin) {
+            System.out.println("Congratulations, " + Cli.getName());
+            countCorrectAnswer = 0;
+        }
+
+    }
+}
