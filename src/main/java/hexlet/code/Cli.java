eeaa7d9d82f518976765f0a3232f4fc15e167ddf
AndreevDan93 <andreevdan93@gmail.com>
2022-07-22 12:57:47 +0500

    fix build

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,13 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void Greeting() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!\n" +
+                "May I have your name?");
+        String name = scanner.next();
+        System.out.println("Hello, " + name + "!");
+    }
 }
