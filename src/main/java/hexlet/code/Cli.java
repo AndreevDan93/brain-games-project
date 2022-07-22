f3d474c7a9b61ab06b389f96d7fabdb1a289bf81
AndreevDan93 <andreevdan93@gmail.com>
2022-07-22 14:08:28 +0500

    lesson 4

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void Greeting() {
+    public static void greeting() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!\n" +
-                "May I have your name?");
+        System.out.println("Welcome to the Brain Games!\n"
+                + "May I have your name?");
         String name = scanner.next();
         System.out.println("Hello, " + name + "!");
     }
