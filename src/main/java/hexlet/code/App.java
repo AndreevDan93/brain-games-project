8e9fceca249fc804e71d3937bc507ec2df68c052
AndreevDan93 <andreevdan93@gmail.com>
2022-07-25 12:43:30 +0500

    add GCD game

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,33 +2,43 @@ package hexlet.code;
 
 import hexlet.code.games.Calculator;
 import hexlet.code.games.EvenCheck;
+import hexlet.code.games.GreatestCommonDivisor;
 
 
 public class App {
     public static void main(String[] args) {
         int inputNum;
+        final int exit = 0;
+        final int greet = 1;
+        final int even = 2;
+        final int calc = 3;
+        final int gcd = 4;
 
         System.out.println("""
                 Please enter the game number and press Enter.
                 1 - Greet
                 2 - Even
                 3 - Calc
+                4 - GCD
                 0 - Exit""");
 
         inputNum = Engine.getScanner().nextInt();
         System.out.println("Your choice: " + inputNum);
 
         switch (inputNum) {
-            case 0:
+            case exit:
                 return;
-            case 1:
+            case greet:
                 Cli.greeting();
                 break;
-            case 2:
+            case even:
                 EvenCheck.game();
                 break;
-            case 3:
+            case calc:
                 Calculator.game();
+                break;
+            case gcd:
+                GreatestCommonDivisor.game();
                 break;
             default:
                 break;
--- src/main/java/hexlet/code/games/GreatestCommonDivisor.java
+++ src/main/java/hexlet/code/games/GreatestCommonDivisor.java
@@ -1,2 +1,36 @@
-package hexlet.code.games;public class GCD {
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class GreatestCommonDivisor {
+    public static void game() {
+        Engine.greeting();
+        System.out.println("Find the greatest common divisor of given numbers.");
+        addArrayToGameData();
+        Engine.gameAlgorithm(Engine.getArrCorrectAnswers(), Engine.getArrQuestions());
+
+    }
+
+    public static void addArrayToGameData() {
+        for (int i = 0; i < Engine.getCountOfRounds(); i++) {
+            int randomNum1 = Engine.getRandomNum(Engine.getRandomRange());
+            int randomNum2 = Engine.getRandomNum(Engine.getRandomRange());
+
+            Engine.getArrCorrectAnswers()[i] = Integer.toString(getGreatestCommonDivisor(randomNum1, randomNum2));
+            Engine.getArrQuestions()[i] = randomNum1 + " " + randomNum2;
+        }
+    }
+
+    public static int getGreatestCommonDivisor(int num1, int num2) {
+        while (num1 != num2) {
+            if (num1 > num2) {
+                num1 = num1 - num2;
+            } else {
+                num2 = num2 - num1;
+            }
+        }
+        return num1;
+    }
+
+
 }
